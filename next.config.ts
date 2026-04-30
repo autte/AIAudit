c7bc43aa3502bada7726ea2aba73a447ec3c53af
autte <auroratsao@hotmail.com>
2026-04-30 13:05:21 -0400

    add windows page

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/windows",
+        destination: "/windows/index.html",
+      },
+    ];
   },
 };
 
